e8f09ff79e5ccd1dc8f9f7134aa2ed8a635e56c0
RIITK SINGH <businessritiksinghdeos@gmail.com>
2026-09-22 13:37:23 +0530

    fix: add SVGProps and style support to DiamondSparkle for Vercel build

--- src/components/shooting-stars.tsx
+++ src/components/shooting-stars.tsx
@@ -27,13 +27,19 @@ interface TwinkleStar {
 }
 
 // 4-Pointed Classic Diamond Star Sparkle (✦)
-function DiamondSparkle({ className }: { className?: string }) {
+function DiamondSparkle({
+  className,
+  style,
+  ...props
+}: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       viewBox="0 0 24 24"
       fill="currentColor"
       className={className}
+      style={style}
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path d="M12 0C12 6.627 6.627 12 0 12C6.627 12 12 17.373 12 24C12 17.373 17.373 12 24 12C17.373 12 12 6.627 12 0Z" />
     </svg>
